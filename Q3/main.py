026165b6399ade72e7715d407af992b6f8b26b4f
Enayat Kareem <75158477+KLAU-S@users.noreply.github.com>
2022-04-09 17:22:16 +0530

    Create main.py

---
+++ Q3/main.py
@@ -0,0 +1,137 @@
+from jax.scipy.special import logsumexp
+from jax.experimental import optimizers
+
+import torch
+from torchvision import datasets, transforms
+
+import time
+from helpers import plot_mnist_examples
+
+batch_size = 100
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=batch_size, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=batch_size, shuffle=True)
+
+
+plot_mnist_examples(train_loader)
+
+def initialize_mlp(sizes, key):
+    """ Initialize the weights of all layers of a linear layer network """
+    keys = random.split(key, len(sizes))
+    # Initialize a single layer with Gaussian weights -  helper function
+    def initialize_layer(m, n, key, scale=1e-2):
+        w_key, b_key = random.split(key)
+        return scale * random.normal(w_key, (n, m)), scale * random.normal(b_key, (n,))
+    return [initialize_layer(m, n, k) for m, n, k in zip(sizes[:-1], sizes[1:], keys)]
+
+layer_sizes = [784, 512, 512, 10]
+# Return a list of tuples of layer weights
+params = initialize_mlp(layer_sizes, key)
+
+def forward_pass(params, in_array):
+    """ Compute the forward pass for each example individually """
+    activations = in_array
+
+    # Loop over the ReLU hidden layers
+    for w, b in params[:-1]:
+        activations = relu_layer([w, b], activations)
+
+    # Perform final trafo to logits
+    final_w, final_b = params[-1]
+    logits = np.dot(final_w, activations) + final_b
+    return logits - logsumexp(logits)
+
+# Make a batched version of the `predict` function
+batch_forward = vmap(forward_pass, in_axes=(None, 0), out_axes=0)
+
+def one_hot(x, k, dtype=np.float32):
+    """Create a one-hot encoding of x of size k """
+    return np.array(x[:, None] == np.arange(k), dtype)
+
+def loss(params, in_arrays, targets):
+    """ Compute the multi-class cross-entropy loss """
+    preds = batch_forward(params, in_arrays)
+    return -np.sum(preds * targets)
+
+def accuracy(params, data_loader):
+    """ Compute the accuracy for a provided dataloader """
+    acc_total = 0
+    for batch_idx, (data, target) in enumerate(data_loader):
+        images = np.array(data).reshape(data.size(0), 28*28)
+        targets = one_hot(np.array(target), num_classes)
+
+        target_class = np.argmax(targets, axis=1)
+        predicted_class = np.argmax(batch_forward(params, images), axis=1)
+        acc_total += np.sum(predicted_class == target_class)
+    return acc_total/len(data_loader.dataset)
+  
+@jit
+def update(params, x, y, opt_state):
+    """ Compute the gradient for a batch and update the parameters """
+    value, grads = value_and_grad(loss)(params, x, y)
+    opt_state = opt_update(0, grads, opt_state)
+    return get_params(opt_state), opt_state, value
+
+# Defining an optimizer in Jax
+step_size = 1e-3
+opt_init, opt_update, get_params = optimizers.adam(step_size)
+opt_state = opt_init(params)
+
+num_epochs = 10
+num_classes = 10
+
+def run_mnist_training_loop(num_epochs, opt_state, net_type="MLP"):
+    """ Implements a learning loop over epochs. """
+    # Initialize placeholder for loggin
+    log_acc_train, log_acc_test, train_loss = [], [], []
+
+    # Get the initial set of parameters
+    params = get_params(opt_state)
+
+    # Get initial accuracy after random init
+    train_acc = accuracy(params, train_loader)
+    test_acc = accuracy(params, test_loader)
+    log_acc_train.append(train_acc)
+    log_acc_test.append(test_acc)
+
+    # Loop over the training epochs
+    for epoch in range(num_epochs):
+        start_time = time.time()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            if net_type == "MLP":
+                # Flatten the image into 784 vectors for the MLP
+                x = np.array(data).reshape(data.size(0), 28*28)
+            elif net_type == "CNN":
+                # No flattening of the input required for the CNN
+                x = np.array(data)
+            y = one_hot(np.array(target), num_classes)
+            params, opt_state, loss = update(params, x, y, opt_state)
+            train_loss.append(loss)
+
+        epoch_time = time.time() - start_time
+        train_acc = accuracy(params, train_loader)
+        test_acc = accuracy(params, test_loader)
+        log_acc_train.append(train_acc)
+        log_acc_test.append(test_acc)
+        print("Epoch {} | T: {:0.2f} | Train A: {:0.3f} | Test A: {:0.3f}".format(epoch+1, epoch_time, train_acc, test_acc))
+
+    return train_loss, log_acc_train, log_acc_test
+
+
+train_loss, train_log, test_log = run_mnist_training_loop(num_epochs, opt_state, net_type="MLP")
+
+# Plot the loss curve over time
+from helpers import plot_mnist_performance
+plot_mnist_performance(train_loss, train_log, test_log, "MNIST MLP Performance")
